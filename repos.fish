f0c2d9f2107d3cd1bd15d11ac0c1794311fe745d
Kayce Basques <kaycebasques@gmail.com>
2026-08-10 04:24:46 +0000

    update submodules

--- repos.fish
+++ repos.fish
@@ -1,9 +1,12 @@
 #!/usr/bin/env fish
 
-set repos "technicalwriting.dev" "kaycebasques.net"
-for repo in $repos
-    if not test -d $HOME/$repo
-        git clone git@github.com:kaycebasques/$repo.git $HOME/$repo
-        git -C $HOME/$repo config --local user.email "kaycebasques@gmail.com"
+set repo_mappings "tw:technicalwriting.dev" "kb:kaycebasques.net"
+for mapping in $repo_mappings
+    set -l parts (string split ":" $mapping)
+    set -l local_name $parts[1]
+    set -l remote_name $parts[2]
+    if not test -d $HOME/$local_name
+        git clone git@github.com:kaycebasques/$remote_name.git $HOME/$local_name
+        git -C $HOME/$local_name config --local user.email "kaycebasques@gmail.com"
     end
 end
